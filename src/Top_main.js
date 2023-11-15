e9b58f2ea7e8fd0ca87046a07534208ee15ef703
joung-pjh <jounghyeon123@gmail.com>
2023-11-15 09:57:55 +0900

    Add wave animation in id #top-main

--- src/Top_main.js
+++ src/Top_main.js
@@ -1,11 +1,41 @@
 import './Top_main.css'
+import React from 'react';
+import Wave from 'react-wavify';
+
+const commonWaveOptions = {
+    height: 10,
+    amplitude: 40,
+    speed: 0.45,
+    points: 3,
+  };
 
 function Top_main() {
     return (
         <>
-        <div class="portfolio-text">포폴의 시작은<br/>여기.</div>
-        <div>
-            <button class="start-button2">시작하기</button>
+        <div id='wave'>
+            <Wave
+              mask="url(#mask)"
+              fill="#1277b0"
+              style={{ display: 'flex' }}
+              options={commonWaveOptions}
+            >
+              <defs>
+                <linearGradient id="gradient" gradientTransform="rotate(90)">
+                  <stop offset="0" stopColor="white" />
+                  <stop offset="0.4" stopColor="black" />
+                </linearGradient>
+                <mask id="mask">
+                  <rect x="0" y="0" width="2000" height="400" fill="url(#gradient)" />
+                </mask>
+              </defs>
+            </Wave>
+         </div>
+
+        <div id='top-sub-main'>
+            <div class="portfolio-text">포폴의 시작은<br/>여기.</div>
+            <div>
+                <button class="start-button2">시작하기</button>
+            </div>
         </div>
         </>
     );
